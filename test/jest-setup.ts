272557328d8d0fc1280a8a78656fd0b71e7d3533
Noel Freitas <noel@adderesolutions.com>
2023-09-26 16:20:40 -0300

    feat: create first test

--- test/jest-setup.ts
+++ test/jest-setup.ts
@@ -1,1 +1,8 @@
-//jest setup
+import { SetupServer } from "@src/server";
+import supertest, { SuperTest } from "supertest";
+
+beforeAll(() => {
+  const server = new SetupServer();
+  server.init();
+  global.testRequest = supertest(server.getApp());
+});
